d81b382162aa88b666f69bbdbecfd04f9b679787
Karim Elgazar <karimelgazar565@gmail.com>
2020-04-21 00:00:39 +0200

    add .py solution

--- 014 Longest Collatz sequence/014.py
+++ 014 Longest Collatz sequence/014.py
@@ -12,3 +12,42 @@ Which starting number, under one million, produces the longest chain?
 NOTE: Once the chain starts the terms are allowed to go above one million.
 
 '''
+
+def longest_collatz(the_end):
+    '''
+    تقوم تلك الدالة بحساب العدد
+    الذى سيأخذ أكبر عدد من الخطوات حتى يصل الى رقم 1 
+    ملاحظة
+    ======
+    الرقم المعطى للدالة غير مضمون فى الحسابات
+    '''
+    
+    the_end = int(the_end)
+    
+    tree = {1:1}
+    for i in range(2, the_end):
+        # عدد الخطوات
+        length = 1 
+        
+        # اخذنا نسخة من الرقم لاننا سنقوم
+        #بتغيير قيمة تلك النشخة بدلا من الرقم الاصلى
+        n = i
+        while n != 1:
+            # تأكد اذا ما كان الرقم قد حسبناه مسبقا أم لا
+            if tree.get(n, False):
+                length += tree[n]
+                break
+            
+            # القاعدة
+            n =  (n // 2) if n % 2 == 0 else (3*n + 1) 
+            length += 1
+
+        tree[i] = length
+    
+#     print(tree)    # ===> للتوضيح فقط
+    
+    # تقوم بارجاع المفتاح صاحب اكبر قيمة فى القاموس 
+    return max(tree, key=tree.get)
+
+
+longest_collatz(1e6)
